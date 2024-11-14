6f930e36f1c08d3fef76662e887ea7972c83defc
Jeet Paresh Mehta <jpmehta@wisc.edu>
2024-11-13 21:16:30 -0600

    Added the UI

--- Digital_Factory/ContentView.swift
+++ Digital_Factory/ContentView.swift
@@ -9,13 +9,93 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+//        ZStack {
+//            // Full-screen background color
+//            Color(.black)
+//                .edgesIgnoringSafeArea(.all) // Ensures the color covers the entire screen
+//            
+//            ZStack{
+//                Rectangle()
+//                        .fill(Color(hex: "#53D9BA"))
+//                        .frame(width: 355,height:180)
+//                        .cornerRadius(10)
+//                
+//                Rectangle()
+//                    .fill(Color(hex: "#E6CFB9"))
+//                    .frame(width: 310,height:150)
+//                    .cornerRadius(10)
+//                
+//            }
+//            .padding(.bottom,440)
+         
+            VStack{
+                HStack(spacing: 25){
+                    RectangleButton(label: "AC")
+                    
+                    RectangleButton(label: "+/-")
+                    
+                    RectangleButton(label: "%")
+                    
+                    RectangleButton(label: "÷")
+                }
+                .padding(.bottom,20)
+                
+                HStack(spacing: 25){
+                    
+                    RectangleButton(label: "7")
+                    
+                    RectangleButton(label: "8")
+                    
+                    RectangleButton(label: "9")
+                    
+                    RectangleButton(label: "×")
+                    
+                }
+                .padding(.bottom,20)
+                HStack(spacing: 25){
+                    RectangleButton(label: "4")
+                    
+                    RectangleButton(label: "5")
+                    
+                    RectangleButton(label: "6")
+                    
+                    RectangleButton(label: "-")
+                    
+                }
+                .padding(.bottom,20)
+                HStack(spacing: 25){
+                    RectangleButton(label: "1")
+                    
+                    RectangleButton(label: "2")
+                    
+                    RectangleButton(label: "3")
+                    
+                    RectangleButton(label: "+")
+                    
+                }
+                .padding(.bottom,20)
+                
+                HStack(spacing: 25){
+                    RectangleButton(label: "0")
+                    
+                    RectangleButton(label: ".")
+                    
+                    Rectangle()
+                    .fill(Color(hex: "#C59C55"))
+                    .frame(width: 165, height: 70)
+                    .cornerRadius(5)
+                    .overlay(
+                        Text("=")
+                            .foregroundColor(.white)
+                            .font(.system(size:30))
+                    )
+                }
+                .padding(.bottom,20)
+                
+            }
+            .padding(.top,300)
+            
         }
-        .padding()
     }
 }
 
---
+++ Digital_Factory/RectangleButton.swift
@@ -0,0 +1,36 @@
+//
+//  SwiftUIView.swift
+//  Digital_Factory
+//
+//  Created by Jeet P Mehta on 13/11/24.
+//
+
+import SwiftUI
+
+struct RectangleButton: View {
+    var label: String?
+    var color: Color = Color(hex: "#3D5E7E")
+    var width: CGFloat = 70
+    var height: CGFloat = 70
+    var action: () -> Void // Action to perform on tap
+
+    var body: some View {
+        Button(action: action) {
+            Rectangle()
+                .fill(color)
+                .frame(width: width, height: height)
+                .cornerRadius(5)
+                .overlay(
+                    Group {
+                        if let label = label {
+                            Text(label)
+                                .foregroundColor(.white)
+                                .fontWeight(.heavy)
+                                .font(.system(size: 22, design: .monospaced))
+                        }
+                    }
+                )
+        }
+    }
+}
+
